dc92f1cc7475f12bdc52227847f058f75ea909b2
cvvlm <michalsk7@gmail.com>
2022-08-13 11:18:10 +0200

    design done and really basic functions in js

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-let number1 = 4;
-let number2 = 2;
-let add = function (a, b) {
-    return a + b;
+const liczby = document.querySelectorAll('.liczba')
+const operator = document.querySelectorAll('.operator')
+const rownosc = document.querySelector('.rownosc')
+const usun = document.querySelector('.usun')
+const wyczysc = document.querySelectorAll('.wyczysc')
+const wynikPoprz = document.querySelector('.poprzednie-dzialanie')
+const wynikAktual = document.querySelector('.aktualne-dzialanie')
+
+var aktualDzialanie = ''
+var poprzDzialanie = ''
+var operacja = undefined
+
+const zaktualizujWynik = () => {
+    wynikAktual.innerText = aktualDzialanie
+    wynikPoprz.innerText = poprzDzialanie
 }
-let substract = function (a, b) {
-    return a - b;
+
+const dodajLiczbe = (liczba) => {
+    if (liczba === ".") {
+        if (aktualDzialanie.includes(".")) {
+            return
+        }
+    }
+    aktualDzialanie = aktualDzialanie.toString() + liczba.toString()
+
 }
-let multiply = function (a, b) {
-    return a * b;
+const usunLiczbe = () => {
+    aktualDzialanie = aktualDzialanie.slice(0, -1)
 }
-let divide = function (a, b) {
-    if (a === 0 || b === 0) {
-        console.log("ERROR")
-    }
-    return a / b;
-}
-function operate(operation) {
-    if (operation === add) {
-        add(number1, number2);
-    }
-    if (operation === substract) {
-        substract(number1, number2);
-    }
-    if (operation === multiply) {
-        multiply(number1, number2);
-    }
-    if (operation === divide) {
-        divide(number1, number2);
-    }
-}
+
+
+liczby.forEach((liczba) => {
+    liczba.addEventListener('click', () => {
+        dodajLiczbe(liczba.innerText)
+        zaktualizujWynik()
+    })
+})
+
+
+usun.addEventListener("click", () => {
+    usunLiczbe()
+    zaktualizujWynik()
+})
+
+operator.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        
+    })
+})
